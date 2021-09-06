fc6fd9f26cd51c3bdace9d6a902ec3657cbaf221
prachisudrik <prachi41024@gmail.com>
2021-09-06 21:03:38 +0530

    1544-Make string great

--- make_string_grt_1544.py
+++ make_string_grt_1544.py
@@ -10,4 +10,21 @@ class Solution:
             else:
                 stack.append(i)
         return "".join(stack)
-                
+
+
+
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s)<=1:
+            return s
+        stack = [s[0]]
+        for i in range(1,len(s)):
+            if len(stack) and abs(ord(stack[-1])-ord(s[i])) == 32:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        return ''.join(stack)
