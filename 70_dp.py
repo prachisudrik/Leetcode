8f4e20464c5cec1c9ad3047ff237c76488fa59d7
prachisudrik <prachi41024@gmail.com>
2022-03-23 13:38:14 -0700

    Solution 2-Finoacci formula

--- 70_dp.py
+++ 70_dp.py
@@ -6,3 +6,10 @@ class Solution:
             one = one + two
             two = temp
         return one 
+
+#Solution 2
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        sqrt5 = sqrt(5)
+        fibn = pow((1+sqrt5)/2,n+1)-pow((1-sqrt5)/2,n+1)
+        return int(fibn/sqrt5)
